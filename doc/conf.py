d8367af152e8373f5371c45c22a3c4ec80d8b05d
Tim Aidan Graulich <63345846+tgraulich@users.noreply.github.com>
2024-10-31 16:49:03 +0100

    Create conf.py

---
+++ doc/conf.py
@@ -0,0 +1,17 @@
+project = "Balmorel GitHub Tutorial"
+copyright = "2024, Tim Aidan Graulich"
+author = "Tim Aidan Graulich"
+release = "0.1"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+extensions = [
+    "myst_parser",  # in order to use markdown
+    "sphinx_tabs.tabs",
+]
+
+myst_enable_extensions = [
+    "colon_fence",  # ::: can be used instead of ``` for better rendering
+]
+
+html_theme = "sphinx_rtd_theme"
